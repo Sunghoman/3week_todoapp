203d7b6f1623a5598851615201a74753bc1e6197
kimsungho <ksh_1035@naver.com>
2022-10-01 16:25:19 +0900

    동적 autofocus

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,7 +2,15 @@ import './style.css'
 
 import List from '../list/List';
 
+import { useRef, useEffect } from 'react';
+
 const Form = ({ posts, setPosts, title, body, onChange, onCreate, onRemove }) => {
+
+  // 처음 input창에 focus를 동적으로 주려고 해본건데 잘 안되넹
+  const inputFocus = useRef(null);
+  useEffect(() => {
+    inputFocus.current.focus();
+  }, []);
 
   const onKeyPress = (e) => {
     if(e.key === 'Enter') {
@@ -15,7 +23,7 @@ const Form = ({ posts, setPosts, title, body, onChange, onCreate, onRemove }) =>
       <div className='input'>
         <div className='input-post'>
           <label for="title">제목</label>
-          <input type="text" autoFocus id="title" name="title" onChange={onChange} value={title} onKeyPress={onKeyPress}/>
+          <input type="text" ref={inputFocus} id="title" name="title" onChange={onChange} value={title} onKeyPress={onKeyPress}/>
 
           <label for="comment">내용</label>
           <input type="text" id="comment" name="body" onChange={onChange} value={body} onKeyPress={onKeyPress}/>
